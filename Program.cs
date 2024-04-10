d7a9eb4169c00334178ae871c65b8aa87d12308c
Subriti <subritiaryal13@gmail.com>
2024-04-10 16:09:46 +0545

    Added Receiver Implementation Project

--- Program.cs
+++ Program.cs
@@ -1,54 +1,57 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
 
-class Program
+namespace RabbitMQProducer
 {
-    static void Main(string[] args)
+    class Program
     {
-        var senderFactory = new ConnectionFactory
+        static void Main(string[] args)
         {
-            HostName = "localhost",
-            UserName = "admin",
-            Password = "admin",
-        };
-        using (var senderConnection = senderFactory.CreateConnection())
-        using (var senderChannel = senderConnection.CreateModel())
-        {
-            DeclareQueue(senderChannel);
-
-            var message = "Hello, RabbitMQ!";
-            SendMessage(senderChannel, message);
-
-            while (true)
+            var senderFactory = new ConnectionFactory
             {
-                Console.WriteLine("\nEnter your message (type 'exit' to quit):");
-                message = Console.ReadLine(); // Accept user input for the message content
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "admin",
+            };
+            using (var senderConnection = senderFactory.CreateConnection())
+            using (var senderChannel = senderConnection.CreateModel())
+            {
+                DeclareQueue(senderChannel);
 
-                if (message.ToLower() == "exit")
-                    break; // Exit the loop if the user types 'exit'
-
+                var message = "Hello, RabbitMQ!";
                 SendMessage(senderChannel, message);
+
+                while (true)
+                {
+                    Console.WriteLine("\nEnter your message (type 'exit' to quit):");
+                    message = Console.ReadLine(); // Accept user input for the message content
+
+                    if (message.ToLower() == "exit")
+                        break; // Exit the loop if the user types 'exit'
+
+                    SendMessage(senderChannel, message);
+                }
             }
         }
-    }
-    static void DeclareQueue(IModel channel)
-    {
-        channel.QueueDeclare(queue: "hello",
-                             durable: false,
-                             exclusive: false,
-                             autoDelete: false,
-                             arguments: null);
-    }
+        static void DeclareQueue(IModel channel)
+        {
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+        }
 
-    static void SendMessage(IModel channel, string message)
-    {
-        byte[] body = Encoding.UTF8.GetBytes(message);
+        static void SendMessage(IModel channel, string message)
+        {
+            byte[] body = Encoding.UTF8.GetBytes(message);
 
-        channel.BasicPublish(exchange: "",
-                             routingKey: "hello",
-                             basicProperties: null,
-                             body: body);
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
 
-        Console.WriteLine("\n [x] Sent {0}", message);
+            Console.WriteLine("\n [x] Sent {0}", message);
+        }
     }
 }
